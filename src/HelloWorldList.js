24cc9db58b97f9ac2fe63c5efa17191a213d5ab5
Dillion <dillion@Dillions-iMac.local>
2018-01-17 15:19:13 -0600

    initial commit

---
+++ src/HelloWorldList.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import './HelloWorldList.css';
+import './AddGreeter';
+
+import HelloWorld from './HelloWorld';
+import AddGreeter from './AddGreeter';
+
+class HelloWorldList extends Component {
+   constructor(props) {
+      super(props);
+      this.state = { greetings: ["Jim", "Dillon", "Bender"] };
+      this.addGreeting = this.addGreeting.bind(this);
+   }
+
+   addGreeting(newName) {
+      this.setState({ greetings: [...this.state.greetings, newName] });
+   }
+
+   renderGreetings() {
+      return this.state.greetings.map(name => (
+         <HelloWorld key={name} name={name} />
+      ));
+   }
+
+   render() {
+      return (
+         <div className="HelloWorldList">
+            <AddGreeter addGreeting={this.addGreeting}/>
+            {this.renderGreetings()}
+         </div>
+      );
+   }
+}
+
+export default HelloWorldList;
